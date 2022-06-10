924412d12c82ddfb5ea6f07fdc82f5ec83e4e47a
_c.h.shin <lee2317884@naver.com>
2022-06-10 23:28:26 +0900

    Create Programmers_1-5.py
    프로그래머스: 모의고사

---
+++ Programmers_1-5.py
@@ -0,0 +1,23 @@
+def solution(answers):
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0,0,0]
+    result = []
+    
+    for i, answer in enumerate(answers):
+        if answer == p1[i%len(p1)]:
+            score[0] += 1
+        if answer == p2[i%len(p2)]:
+            score[1] += 1
+        if answer == p3[i%len(p3)]:
+            score[2] += 1
+    
+    for i, s in enumerate(score):
+        if s == max(score):
+            result.append(i+1)
+        
+    return result
+
+a = [1,2,3,4,5]
+print(solution(a))	
