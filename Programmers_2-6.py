abb72d89cbe4b042aecd4c29340d9f00857e93f0
_c.h.shin <lee2317884@naver.com>
2022-06-23 16:54:29 +0900

    프로그래머스:

---
+++ Programmers_2-6.py
@@ -0,0 +1,14 @@
+def solution(phone_book) :
+    answer = True
+    dic = {}
+    
+    for p in phone_book:
+        dic[p] = 1
+        
+    for p in phone_book:
+        tmp = ''
+        for num in p :
+            tmp += num
+            if tmp in dic and tmp != p:
+                answer = False
+    return answer
